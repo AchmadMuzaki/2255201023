0fccf662dbf7701e34f9d334e5f4263b856961be
AchmadMuzaki <145527911+AchmadMuzaki@users.noreply.github.com>
2023-10-20 06:13:33 +0700

    Pertemuan2

---
+++ pertemuan2/Runmobil.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan2;
+
+/**
+ *
+ * @author asus
+ */
+public class Runmobil {
+   
+    public static void main(String[] args) {
+        Mastermobil Mercy = new Mastermobil();
+    
+    System.out.println("Ini adalah mobil Mercy");
+    System.out.println("Warna Mercy: " +Mercy.warnamobil);
+    System.out.println("Merek Mercy: "+Mercy.merkmobil);
+    System.out.println("Tahun Pembuatan: "+Mercy.tahunmobil);            
+    }
+}
